0eb4db42c652e33d721d217ac45647f648000834
George Richards <g.richards101@gmail.com>
2018-04-12 16:25:08 +0100

    catch up with main repo

--- server/server.js
+++ server/server.js
@@ -1,19 +1,41 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
-const app = express();
 const port = process.env.PORT || 3000;
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
 
 app.use(express.static(publicPath));
 
-// console.log(__dirname + '/../public');
-// console.log(publicPath);
+io.on('connection', (socket) => {
+  console.log('New user connected');
 
-// app.get('/', (req, res) => {
-//   res.render('index.html');
-// });
+  // socket.emit('newMessage', { // emits to one connection
+  //   from: 'George',
+  //   text: 'Hello!',
+  //   createdAt: 28395
+  // });
 
-app.listen(port, () => {
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+
+    io.emit('newMessage', { // emits to all connections
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+server.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
